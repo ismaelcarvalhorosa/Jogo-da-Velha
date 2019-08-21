169e24837a20732b1f89f05115e6e27bb92f5b62
ismael-mogo <dt65estudio@yahoo.com.br>
2019-08-21 15:11:15 -0300

    escreve os simbolas e valida

---
+++ script/jogoVelha.js
@@ -0,0 +1,63 @@
+const tic_tac_toe = {
+    board:['','','','','','','','',''],
+    simbols: {
+       options:['x','o'],
+       turn_index:0, 
+       change: function(){
+           this.turn_index = (this.turn_index === 0 ? 1 :0);
+       }
+    },
+        container_element:null,
+        gameover:false,
+        vense_sequences:[
+            [1,2,3],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ],
+
+    init: function(constainer){
+        this.container_element = constainer;
+    },
+
+    make_play: function(position){
+        if(this.gameover)return false;
+        if(this.board[position] === ''){
+            this.board[position] = this.simbols.options [this.simbols.turn_index];
+            this.draw(); 
+            let vense_sequences_index = this.check_sequece(this.simbols.options [this.simbols.turn_index]) 
+            if(this.vense_sequences_index >=0){
+
+            }else{
+                this.simbols.change();
+            }
+            return true;
+        }else{
+            false;
+        }
+    },
+
+    check_sequece:function(simbols){
+        for(i in this.vense_sequences){
+            if(this.board[this.vense_sequences[i][0]]== simbols &&
+               this.board[this.vense_sequences[i][1]]== simbols &&
+               this.board[this.vense_sequences[i][2]]== simbols){
+                   return i ;
+                   console.log('Sequecia vencendore:' + i);
+               }
+        }
+    },
+    draw: function(){
+        let content ='';
+
+        for(i in this.board){
+            content += '<div onclick="tic_tac_toe.make_play(' + i + ')">' + i + '</div>';
+        }
+        this.container_element.innerHTML = content;
+    }
+
+};  
